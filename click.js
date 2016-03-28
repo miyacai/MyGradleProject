e40d122fe427853c3613950c3295081911046637
miya cai <miya.cai@blackboard.com>
2016-03-28 14:32:54 +0800

    js

---
+++ click.js
@@ -0,0 +1,11 @@
+function toClick() {
+			$.ajax({
+				url : "http://localhost:8080/myProject/api/v1/print/mybatis"
+			}).done(function(data) {
+				var text = "<tr><th>name</th><th>age</th><th>sex</th><tr>";
+				for(var i=0;i<data.length;i++){
+					text = text + "<tr><td>" + data[i].name + "</td>"+"<td>" + data[i].age + "</td>" + "<td>" + data[i].sex + "</td><tr>";
+				}
+				$("#table").html(text);
+			});
+		}
