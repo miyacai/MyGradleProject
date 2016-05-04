9a0151a823d80e5004a2040fbd842f73802e6cf6
miya cai <miya.cai@blackboard.com>
2016-05-04 23:14:36 +0800

    log4j2 test

---
+++ src/main/java/org/gradle/interceptor/MyInterceptor.java
@@ -0,0 +1,34 @@
+package org.gradle.interceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+public class MyInterceptor implements HandlerInterceptor{
+	private static final Logger LOG = LogManager.getLogger("MyAccessLog");
+
+	public boolean preHandle(HttpServletRequest request,
+			HttpServletResponse response, Object handler) throws Exception {
+		LOG.info("access log is" + request.getPathInfo());
+		return false;
+	}
+
+	public void postHandle(HttpServletRequest request,
+			HttpServletResponse response, Object handler,
+			ModelAndView modelAndView) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void afterCompletion(HttpServletRequest request,
+			HttpServletResponse response, Object handler, Exception ex)
+			throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
--- src/main/java/org/gradle/service/PersonServiceImpl.java
+++ src/main/java/org/gradle/service/PersonServiceImpl.java
@@ -2,6 +2,8 @@ package org.gradle.service;
 
 import java.util.List;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.gradle.mapper.PersonMapper;
 import org.gradle.modle.Person;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,12 +12,21 @@ import org.springframework.stereotype.Service;
 
 @Service("personService")
 public class PersonServiceImpl {
+	
+    private static final Logger LOG = LogManager.getLogger(PersonServiceImpl.class);
 
 	@Autowired
 	protected PersonMapper personMapper;
 	
 	public List<Person> getAllPersons() {
+		LOG.trace("trace message");
+		LOG.debug("debug message");
+		LOG.info("info message");
+		LOG.warn("warn message");
+		LOG.error("error message");
+		LOG.fatal("fatal message");
+		System.out.println("Hello World!");
 		return personMapper.getAllPersons();
 	}
-
+	
 }
